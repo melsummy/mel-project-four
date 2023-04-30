7252644eccb9cc5706545ab3bc68a53eba4edbb2
melsummy <mjfischer2@madisoncollege.edu>
2023-04-30 00:00:35 -0500

    modified src/App.js to include number counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [counter, setCounter] = useState(0)
+
+  const increment = () => {
+    setCounter(counter + 1);
+  }
+  const decrement = () => {
+    setCounter(counter - 1);
+  }
+  
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hi
+          Counter: {counter} <button onClick={increment} type='button'>+</button><button onClick={decrement} type='button'>-</button>
         </p>
         <a
           className="App-link"
